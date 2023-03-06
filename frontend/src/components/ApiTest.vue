dbb23f76ef1e0d20e8b5ed7ee3ace0e1e1daa44b
Ogunsakin Damilola <ogunsakindamilola@roam-191-97.net.brad.ac.uk>
2023-03-06 16:12:57 +0000

    Frontend updates

--- frontend/src/components/ApiTest.vue
+++ frontend/src/components/ApiTest.vue
@@ -10,7 +10,8 @@ export default {
 
   methods: {
     async fetchData() {
-      const url = 'http://localhost/'
+      const userId = 1
+      const url = "http://localhost/get-user-weather/" + userId;
       this.apiResponse = await (await fetch(url)).json()
     }
   }
@@ -20,6 +21,9 @@ export default {
 <template>
   <div v-if="!apiResponse">
     Pinging the api...
+    <h1 class="text-3xl font-light underline">
+      Hello world!
+    </h1>
   </div>
 
   <div v-if="apiResponse">
@@ -27,5 +31,11 @@ export default {
     <code>
     {{ apiResponse }}
     </code>
+  </div>
+
+  <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6">
+    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+
+    </div>
   </div>
 </template>
