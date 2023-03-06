00e815dea01c190469f72f57f624b40dd74e0151
Ogunsakin Damilola <ogunsakindamilola@roam-191-97.net.brad.ac.uk>
2023-03-06 12:53:34 +0000

    Updated test

--- api/tests/Unit/ViewUserWeatherServiceTest.php
+++ api/tests/Unit/ViewUserWeatherServiceTest.php
@@ -3,34 +3,49 @@
 namespace Tests\Unit;
 
 use App\Http\Services\ViewUserWeatherService;
+use App\Jobs\HandleUserWeatherJob;
 use App\Models\User;
+use Illuminate\Support\Facades\Queue;
 use Tests\TestCase;
 
 class ViewUserWeatherServiceTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_that_user_without_weather_is_viewed()
+    private User $user;
+
+    public function setUp(): void
     {
-        $user = User::factory()->create();
-        $response = (new ViewUserWeatherService($user->id))->handle();
+        parent::setUp();
+        $this->setupUser();
+    }
+
+    public function test_that_user_without_weather_can_be_viewed()
+    {
+        $response = (new ViewUserWeatherService($this->user->id))->handle();
         $this->assertTrue($response['success']);
         $this->assertEquals(201, $response['code']);
         $this->assertEquals('Fetching an updated weather report', $response['message']);
     }
 
+    public function test_that_weather_handle_job_is_dispatched()
+    {
+        Queue::fake();
+        (new ViewUserWeatherService($this->user->id))->handle();
+        Queue::assertPushed(HandleUserWeatherJob::class);
+    }
 
-    public function test_that_user_with_weather_data_is_viewed(){
-        $user = User::query()->orderBy('id', 'desc')->first();
-        $response = (new ViewUserWeatherService($user->id))->handle();
+
+    public function test_that_user_with_weather_data_can_be_viewed(){
+        $this->user = User::first();
+        $response = (new ViewUserWeatherService($this->user->id))->handle();
         $this->assertTrue($response['success']);
         $this->assertIsArray($response['data']);
         $expectedKeys = ['coord', 'weather', 'base', 'main', 'visibility', 'wind', 'clouds', 'dt', 'sys', 'timezone', 'id', 'name', 'cod'];
         foreach ($expectedKeys as $expectedKey){
             $this->assertArrayHasKey($expectedKey, $response['data']);
         }
+    }
+
+    private function setupUser(){
+        $this->user = User::factory()->create();
     }
 }
