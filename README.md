90f36e4b7cfb0deae0f328d828cdd21ec94a5e07
Ogunsakin Damilola <ogunsakindamilola@Black-Rain-2.local>
2023-03-07 05:42:30 +0000

    Updated readme && test envinronment

--- README.md
+++ README.md
@@ -1,35 +1,14 @@
-# Fullstack Challenge
+# Fullstack Challenge Solution
 
-## Instructions
-Using Laravel and VueJS, create an application which shows the weather for a set of users.
-- Clone this repository. 
-- Once completed, send a link of the clone repository to interviewer and let them know how long the exercise took. 
-- Update the frontend landing page to show a list of users and their current weather.
-- Clicking a user opens a modal or screen which shows that users detailed weather report.
-- Weather update should be no older than 1 hour.
-- Internal API request(s) to retrieve weather data should take no longer than 500ms. Consider that external APIs could and will take longer than this from time to time and should be accounted for. 
-- We are looking for attention to detail!
-- Instructions are purposely left somewhat open-ended to allow the developer to make some of their own decisions on implementation and design. 
-- This is not a designer test so the frontend does not have to look "good", but of course bonus points if you can make it look appealing. 
-
-## Things to consider:
-- Chose your own weather api such as https://openweathermap.org/api or https://www.weather.gov/documentation/services-web-api.
-- Testability.
-- Best practices.
-- Design patterns.
-- Availability of external APIs is not guaranteed and should not cause page to crash.
-- Twenty randomized users are added via the seeder process, each having their own unique location (longitude and latitude).
-- Redis is available (Docker service) if you wish to use it.
-- Queues, workers, websockets could be useful.
-- Feel free to use a frontend UI library such as PrimeVue, Vuetify, Bootstrap, Tailwind, etc. 
-- Anything else you want to do to show off your coding chops!
-
-## To run the local dev environment:
+## To run the local dev environment: (Updated)
 
 ### API
 - Navigate to `/api` folder
 - Ensure version docker installed is active on host
 - Copy .env.example: `cp .env.example .env`
+- Sign up on OpenWeather(https://openweathermap.org/api ) to retrieve API key and set in env as `WEATHER_API_KEY=`
+- Stop any running docker containers `docker compose down`
+- Build new docker containers `docker compose build`
 - Start docker containers `docker compose up` (add `-d` to run detached)
 - Connect to container to run commands: `docker exec -it fullstack-challenge-app-1 bash`
   - Make sure you are in the `/var/www/html` path
--- frontend/src/components/ApiTest.vue
+++ frontend/src/components/ApiTest.vue
@@ -1,41 +1,183 @@
 <script>
+import iziToast from "izitoast";
 export default {
   data: () => ({
-    apiResponse: null
+    users: [],
+    weatherList: [],
+    baseUrl: "http://localhost/",
+    showModal: false,
+    modalWeather: {},
+    modalUser: {},
   }),
 
   created() {
-    this.fetchData()
+    this.fetchData();
+    // const pusher = new Pusher("6b16e5b60b40dc1cd51b", {
+    //   cluster: "eu",
+    // });
+    //
+    // const channel = pusher.subscribe("weather");
+    // channel.bind("weather", function (data) {
+    //   console.log(data);
+    //   console.log("log lol");
+    // });
   },
 
   methods: {
     async fetchData() {
-      const userId = 1
-      const url = "http://localhost/get-user-weather/" + userId;
-      this.apiResponse = await (await fetch(url)).json()
-    }
-  }
-}
+      const url = this.baseUrl + "users";
+      const response = await (await fetch(url)).json();
+      if (response.success) {
+        this.users = response.data;
+        iziToast.success({
+          title: "Success",
+          message: response.message,
+        });
+      }
+      iziToast.error({
+        title: "Error",
+        message: response.message,
+      });
+    },
+
+    async fetchUserWeather(user) {
+      const userId = user.id
+      const url = this.baseUrl + "get-user-weather/" + userId;
+      const response = await (await fetch(url)).json();
+      if (response.success && response.code === 200) {
+        this.weatherList.forEach((weather, index) => {
+          if (weather.user_id === userId) {
+            this.weatherList.splice(index, 1);
+          }
+        });
+        let weather = {
+          user_id: userId,
+          details: response.data,
+        };
+        this.weatherList.push(weather);
+        this.modalWeather = weather;
+        this.modalUser = user;
+        this.showModal = true;
+        iziToast.success({
+          title: "Success",
+          message: response.message,
+        });
+      } else if (response.success && response.code === 201) {
+        iziToast.info({
+          title: "Message",
+          message: response.message,
+        });
+      } else if (!response.success) {
+        iziToast.error({
+          title: "Error",
+          message: response.message,
+        });
+      }
+    },
+
+    closeModal() {
+      this.showModal = false;
+      this.modalWeather = {};
+      this.modalUser = {};
+    },
+  },
+};
 </script>
 
 <template>
-  <div v-if="!apiResponse">
-    Pinging the api...
-    <h1 class="text-3xl font-light underline">
-      Hello world!
-    </h1>
-  </div>
-
-  <div v-if="apiResponse">
-    The api responded with: <br />
-    <code>
-    {{ apiResponse }}
-    </code>
-  </div>
-
   <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6">
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+    <div
+      class="w-full mt-6 px-6 py-4 bg-gray-800 shadow-md overflow-hidden sm:rounded-lg"
+    >
+      <div class="mx-auto">
+        <div class="bg-white p-4 rounded-lg shadow-md">
+          <div class="overflow-auto">
+            <table class="table-auto w-full">
+              <thead>
+                <tr class="bg-gray-200">
+                  <th class="px-4 py-2">ID</th>
+                  <th class="px-4 py-2">User</th>
+                  <th class="px-4 py-2">Action</th>
+                </tr>
+              </thead>
+              <tbody v-if="users.length > 0">
+                <tr class="bg-white" v-bind:key="user.id" v-for="user in users">
+                  <td class="border px-4 py-2">{{ user.id }}</td>
+                  <td class="border px-4 py-2">{{ user.name }}</td>
+                  <td class="border px-4 py-2">
+                    <button
+                      class="bg-blue-500 hover:bg-blue-600 text-sm px-2 py-1 text-white rounded-md"
+                      @click="fetchUserWeather(user)"
+                    >
+                      View Weather
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+              <tbody v-else>
+                <tr class="bg-white">
+                  <td class="border px-4 py-2 text-center" colspan="3">
+                    No user data yet
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div
+      class="relative z-10"
+      aria-labelledby="modal-title"
+      role="dialog"
+      v-if="showModal"
+      aria-modal="true"
+    >
+      <div
+        class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+      ></div>
+      <div class="fixed inset-0 z-10 overflow-y-auto">
+        <div
+          class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0"
+        >
+          <div
+            class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg"
+          >
+            <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+              <h3 class="text-2xl font-bold">{{ modalUser.name }} <small class="text-sm font-medium text-gray-600">{{ modalWeather.details.last_updated_at }}</small></h3>
+              <blockquote>
+                <p class="font-medium text-base text-gray-700">
+                  {{ modalWeather.details.weather[0].main }}
+                  <small>({{ modalWeather.details.weather[0].description }})</small>
+                </p>
+              </blockquote>
+              <div class="text-blue-500 text-xs">
+                Temp: {{ modalWeather.details.main.temp }}
+              </div>
+              <div class="text-gray-500 text-xs">
+                Min Temp: {{ modalWeather.details.main.temp_min }}
+              </div>
+              <div class="text-gray-500 text-xs">
+                Max Temp: {{ modalWeather.details.main.temp_max }}
+              </div>
+              <div class="text-xs">
+                Lat:{{ modalWeather.details.coord.lat }} Long:{{ modalWeather.details.coord.lon }}
+              </div>
+            </div>
+            <div
+              class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6"
+            >
 
+              <button
+                class="mt-3 inline-flex w-full justify-center rounded-md border bg-blue px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                @click="closeModal"
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
